5f12059c3205b93ec8e041820070b44432b3c24f
B_R <b.rajabov@eskhata.tj>
2024-11-04 16:28:45 +0500

    add black formatter and apply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 import uvicorn
 from items_views import router as items_router
 from users.views import router as users_router
@@ -11,24 +10,18 @@ app.include_router(users_router)
 
 @app.get("/")
 def hello_index():
-    return {
-        "message": "Hello index!"
-    }
+    return {"message": "Hello index!"}
+
 
 @app.get("/hello/")
-def hello_world(name:str = "World"):
+def hello_world(name: str = "World"):
     name = name.strip().title()
-    return { "name": f"Hello, {name}!" }
-
+    return {"name": f"Hello, {name}!"}
 
 
 @app.get("/calc/add/")
 def add(a: int, b: int):
-    return {
-        "a": a,
-        "b": b,
-        "result": a + b
-    }
+    return {"a": a, "b": b, "result": a + b}
 
 
 if __name__ == "__main__":
