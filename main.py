7410eefa43b79fa5da15460d723bcf484f334e3a
B_R <b.rajabov@eskhata.tj>
2024-11-05 15:31:56 +0500

    Implement CRUD operations with products

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+from contextlib import asynccontextmanager
+
+from core.config import settings
+from core.models import Base, db_helper
 from fastapi import FastAPI
 import uvicorn
 from items_views import router as items_router
 from users.views import router as users_router
+from api_v1 import router as router_v1
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router, tags=["items"])
 app.include_router(users_router)
 
