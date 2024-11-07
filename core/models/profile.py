92e93e7ea190628d5da797d1f7e25c6c93af17f2
B_R <b.rajabov@eskhata.tj>
2024-11-07 09:09:11 +0500

    create profile for user

---
+++ core/models/profile.py
@@ -0,0 +1,17 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40), unique=False)
+    last_name: Mapped[str | None] = mapped_column(String(40), unique=False)
+    bio: Mapped[str | None]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
